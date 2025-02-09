03615f77f17d058cec529d262e187de178aaef94
Esteban Vallejo Morales <esteban.vallejomo@gmail.com>
2025-02-09 15:32:11 -0500

    First develop commit.

--- StoreApp/StoreApp.Api/Program.cs
+++ StoreApp/StoreApp.Api/Program.cs
@@ -10,6 +10,7 @@ namespace StoreApp.Api
             // Add services to the container.
 
             builder.Services.AddControllers();
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
